dada81537668b60c197db1bca0b3e8f82b62a1a1
Erby77 <160023462+Erby77@users.noreply.github.com>
2024-11-02 14:23:58 +0100

    je push ma quete 3 dans components.2

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,11 +1,28 @@
 const PokemonCard = () => {
+	const pokemon = [
+		{
+			name: "Bulbasaur",
+			imgSrc:
+				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+		},
+		{
+			name: "Charamander",
+			imgSrc:
+				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+		},
+		{
+			name: "mew",
+		},
+	];
+
 	return (
 		<figure>
-			<img
-				src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-				alt="bulbasaur"
-			/>
-			<h1>Bulbasaur</h1>
+			{pokemon[0].imgSrc !== undefined ? (
+				<img src={pokemon[0].imgSrc} alt={pokemon[0].name} />
+			) : (
+				<p>???</p>
+			)}
+			<h1>{pokemon[0].name}</h1>
 		</figure>
 	);
 };
